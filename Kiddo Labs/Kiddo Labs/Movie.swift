142154b3c951d0c783d0f1b30b360b989d5eef09
Lucas Firmo <lucas.firmo@tinitell.com>
2016-08-20 15:48:04 -0300

    Created Movie model

---
+++ Kiddo Labs/Kiddo Labs/Movie.swift
@@ -0,0 +1,26 @@
+//
+//  Movie.swift
+//  Kiddo Labs
+//
+//  Created by Lucas Domene Firmo on 8/20/16.
+//  Copyright © 2016 Domene. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    
+    // MARK: Attributes
+    
+    var id: Int
+    var title: String
+    var year: Int
+    
+    // MARK: - Initializer
+    
+    init(id: Int, title: String, year: Int) {
+        self.id = id
+        self.title = title
+        self.year = year
+    }
+}
