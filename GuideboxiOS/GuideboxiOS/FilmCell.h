d9cfebbdbae2fdee76c8ff52d7ed9627e328e989
Ezequiel Thomazetto <ezequielt@ciandt.com>
2016-09-08 20:47:39 -0300

    ui setup

---
+++ GuideboxiOS/GuideboxiOS/FilmCell.h
@@ -0,0 +1,13 @@
+//
+//  FilmCell.h
+//  GuideboxiOS
+//
+//  Created by Ezequiel Thomazetto on 08/09/16.
+//  Copyright © 2016 Ezequiel Thomazetto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilmCell : UICollectionViewCell
+
+@end
---
+++ GuideboxiOS/GuideboxiOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GuideboxiOS
+//
+//  Created by Ezequiel Thomazetto on 08/09/16.
+//  Copyright © 2016 Ezequiel Thomazetto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
